c93488efbfa28b91063b0cbfa1de68812e0454d0
Phillip Lakis <p.lakis@icloud.com>
2021-01-19 10:56:42 +1100

    Initial Commit

---
+++ Shared/mNRLApp.swift
@@ -0,0 +1,17 @@
+//
+//  mNRLApp.swift
+//  Shared
+//
+//  Created by Phillip Lakis on 19/1/21.
+//
+
+import SwiftUI
+
+@main
+struct mNRLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
